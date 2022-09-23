58826576d6e213a4ff895eb2f25774c274dc53cf
Andrew Zhang <andrew.zhang@westpoint.edu>
2022-09-22 20:22:21 -0400

    CFG

--- hw3.scala
+++ hw3.scala
@@ -15,3 +15,39 @@ object hw3 extends eecs.cs478:
   /***************** Proof by Structural Induction Problem ***********************/
 
   /***************** Language Creation, Phase 3 ***********************/
+  /*<type> = "int"|"float"|"char"|"string"|"Bool"
+  <variable> = (a-z)((a-z)|(A-Z)|(0-9)|_)*
+  <operator> = '+'|'-'|'*'|'/'|'^'|'%'
+  <statement> = <declaration> | <assignment> | <function> | <expression> | <loop> | <if> | <return>
+  <declaration> = (<type> <variable>) | (<type> <assignment>)
+  <assignment> = <variable> '=' (<literal> | <expression>)
+  <function> = <type> <variable>'('( <declaration> ',')* <declaration>)?')''{'<statement>+'}'
+
+  -----------------------Addresses ambiguity of <expression> from part B-----------------------
+
+  <expression0> = <literal> | <variable> | '(' <expression4> ')'
+  <expression1> = <expression1> '%' <expression0> | <expression0>
+  <expression2> = <expression2> '^' <expression1> | <expression1>
+  <expression3> = <expression3> ('/'|'*') <expression2> | <expression2>
+  <expression4> = <expression4> ('-'|'+') <expression3> | <expression3>
+
+  ----------------------------------------------------------------------------------------------
+
+  <loop> = <for> | <while>
+  <for> = "for" '(' <assignment> ',' <conditional> ',' <modification> ')' '\n' '{' <statement>+ '}'
+  <modification> = <variable> ('+'|'-'|'*'|'/')'=' <literal> | <variable>("++"|"--")
+  <while> = "while" '('<conditional>')' '\n' '{'<statement>+'}'
+
+  -----------------------Addresses ambiguity of <conditional> from part B-----------------------
+
+  <conditional0> = <expr><comp><expr> | '(' <conditional2> ')'
+  <conditional1> = <conditional1> "&&" <conditional0> | <conditional0>
+  <conditional2> = <conditional2> "||" <conditional1> | <conditional1>
+
+  ----------------------------------------------------------------------------------------------
+  <comp> = "=="|"!="|'>'|'<'|">="|"<="
+  <if> = "if" '('<conditional>')' '\n' '{' <statement>+ '}' '\n' (<else if> '\n')* <else>?
+  <else if> = "else if" '('<conditonal>')' '\n' '{' <statement>+ '}'
+  <else> = "else" '\n' <statement>+
+  <return> = "return" <expr>
+  */
